7cc91e2e8dc50d7a91bbcf46a581cffd9d1653d8
sky11fur <sky11fur@gmail.com>
2021-03-31 13:27:31 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip install keras==2.0.8 || exit 1
-pip install tensorflow==1.14.0 || exit 1
 pip install numpy || exit 1
 pip install scipy==1.1.0 || exit 1
+pip install tensorflow'>='1.14.0 || exit 1
